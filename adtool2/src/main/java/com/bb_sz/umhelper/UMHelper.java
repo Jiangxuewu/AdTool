bf39c1dd0a48265e75d0a194484046b0e58a66d3
sky <sky-dev@bb-sz.com>
2017-07-11 11:03:04 +0800

    解决umeng服务器返回乱码

--- adtool2/src/main/java/com/bb_sz/umhelper/UMHelper.java
+++ adtool2/src/main/java/com/bb_sz/umhelper/UMHelper.java
@@ -31,6 +31,7 @@ import javax.mail.MessagingException;
 
 public class UMHelper {
 
+    private static final String ENCODING = "utf-8";
     public static String cookie = "um_lang=zh; pgv_pvi=529788928; cn_f7169cbd4377cl2a77d3_dplus=%7B%22distinct_id%22%3A%20%22156964f68d3777-0bee8f2a236d0f-404c0628-1fa400-156964f68d48ea%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201478248735%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201478248735%7D; cn_3e15a5f04a2742ae1e32_dplus=%7B%22distinct_id%22%3A%20%2215905ad30e925c-0208b50ba9f5de-6a191178-1fa400-15905ad30ea165%22%2C%22%E6%98%AF%E5%90%A6%E7%99%BB%E5%BD%95%22%3A%20%22%E5%B7%B2%E7%99%BB%E5%BD%95%22%2C%22%E7%94%A8%E6%88%B7%E5%90%8D%2B%E6%98%B5%E7%A7%B0%22%3A%20%22304261930%40qq.com%2B304261930%40qq.com%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201481858959%2C%22initial_view_time%22%3A%20%221481858943%22%2C%22initial_referrer%22%3A%20%22https%3A%2F%2Fi.umeng.com%2Fuser%2Fproducts%3Fspm%3D0.0.0.0.U9SKGo%22%2C%22initial_referrer_domain%22%3A%20%22i.umeng.com%22%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201481858959%7D; cn_1255795631_dplus=%7B%22distinct_id%22%3A%20%22159ac052e5d98e-01cded676d1d7b-6a191178-1fa400-159ac052e5e620%22%7D; cn_session_id_e41519ea7a5b8g5d6b18=1484649741; cn_e41519ea7a5b8g5d6b18_dplus=%7B%22distinct_id%22%3A%20%22159ac05384bab-0ecba95ea38e2a-6a191178-1fa400-159ac05384c61%22%2C%22%24initial_time%22%3A%201484620941%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fadplus.umeng.com%2F%3Fclose%3D1%26show_protocol%3D1%26key%3D97eedbabfd2bb89e50307cd0277dbd86_1484649734_12525%22%2C%22%24initial_referring_domain%22%3A%20%22adplus.umeng.com%22%7D; l=Ajc335qv4AuAduy02WKaEbGJRyGANQte; _ga=GA1.2.310020993.1475132570; cn_1259827933_dplus=%7B%22distinct_id%22%3A%20%221596c5c035b6c2-098c9767d713a2-6a191178-1fa400-1596c5c035c916%22%2C%22initial_view_time%22%3A%20%221494468706%22%2C%22initial_referrer%22%3A%20%22http%3A%2F%2Fwww.umeng.com%2F%22%2C%22initial_referrer_domain%22%3A%20%22www.umeng.com%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201497496401%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201497496401%7D; cn_ab16623e58941o0088a4_dplus=%7B%22distinct_id%22%3A%20%22159ac041c1ea45-0f60aa71339269-6a191178-1fa400-159ac041c1fca%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201498128353%2C%22utm_source%22%3A%20%22zzbanner12%22%2C%22initial_view_time%22%3A%20%221484648279%22%2C%22initial_referrer%22%3A%20%22http%3A%2F%2Fwww.umeng.com%2F%22%2C%22initial_referrer_domain%22%3A%20%22www.umeng.com%22%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201498128353%7D; cn_1260769985_dplus=%7B%22distinct_id%22%3A%20%2215901a0288e72-0eda2150c4b98f-6a191178-1fa400-15901a0288fa66%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201498128353%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201498128353%7D; umlid_5938b4561c5dd077360011ea=20170706; __utmt=1; CNZZDATA1258498910=22763969-1471396564-%7C1499304157; CNZZDATA1253424029=193568835-1475135190-http%253A%252F%252Fmobile.umeng.com%252F%7C1499304043; umlid_5490f600fd98c52678000244=20170706; __utma=151771813.431653530.1471400505.1499218906.1499308322.377; __utmb=151771813.12.10.1499308322; __utmz=151771813.1471400505.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); CNZZDATA1259864772=172728588-1471396554-http%253A%252F%252Fmobile.umeng.com%252F%7C1499306767; cn_1259864772_dplus=%7B%22distinct_id%22%3A%20%22156964e09a99b-01aeb52aa7e63f-404c0628-1fa400-156964e09aa804%22%2C%22initial_view_time%22%3A%20%221471396554%22%2C%22initial_referrer%22%3A%20%22http%3A%2F%2Fmobile.umeng.com%2Fapps%2Feec0005e66e85e76d8ac3b75%2Fappkey%22%2C%22initial_referrer_domain%22%3A%20%22mobile.umeng.com%22%2C%22%24recent_outside_referrer%22%3A%20%22%24direct%22%2C%22sp%22%3A%20%7B%22%E6%98%AF%E5%90%A6%E7%99%BB%E5%BD%95%22%3A%20true%2C%22USER%22%3A%20%22304261930%40qq.com%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201499308657%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201499308657%7D%7D; cn_1258498910_dplus=%7B%22distinct_id%22%3A%20%221596c5c035b6c2-098c9767d713a2-6a191178-1fa400-1596c5c035c916%22%2C%22initial_view_time%22%3A%20%221483581671%22%2C%22initial_referrer%22%3A%20%22http%3A%2F%2Fwww.bb-sz.com%2F%22%2C%22initial_referrer_domain%22%3A%20%22www.bb-sz.com%22%2C%22sp%22%3A%20%7B%22%24recent_outside_referrer%22%3A%20%22www.bb-sz.com%22%2C%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201499308673%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201499308673%7D%7D; cna=Y1a3DzkWARgCAXeJIHBSUA7U; umplusappid=umcenter; __ufrom=http://www.umeng.com/; cn_a61627694930aa9c80cf_dplus=%7B%22distinct_id%22%3A%20%22156964a9ea7402-09002262c00f71-404c0628-1fa400-156964a9ea863b%22%2C%22initial_view_time%22%3A%20%221471399450%22%2C%22initial_referrer%22%3A%20%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DIE3uJwCXoZLu8mjVBAxgAJQUu7QO0LjlNAfk8ysWkJK%26wd%3D%26eqid%3D8d840c93001011380000000657b3c959%22%2C%22initial_referrer_domain%22%3A%20%22www.baidu.com%22%2C%22%E7%94%A8%E6%88%B7%E5%90%8D%22%3A%20%22304261930%40qq.com%22%2C%22sp%22%3A%20%7B%22%24_sessionid%22%3A%200%2C%22%24_sessionTime%22%3A%201499308658%2C%22%24dp%22%3A%200%2C%22%24_sessionPVTime%22%3A%201499308658%2C%22%24recent_outside_referrer%22%3A%20%22%24direct%22%7D%7D; UM_distinctid=156964a9ea7402-09002262c00f71-404c0628-1fa400-156964a9ea863b; umengplus_name=304261930%40qq.com; umplusuuid=882b83d165df8e17ee9f82bbe5d39015; isg=AkhIJ7QchTac1-dce42C5zVeGbZQZ6OP_jy9SAL5DkO23epHqwN0ik2fIYNW; ummo_ss=BAh7CUkiGXdhcmRlbi51c2VyLnVzZXIua2V5BjoGRVRbCEkiCVVzZXIGOwBGWwZvOhNCU09OOjpPYmplY3RJZAY6CkBkYXRhWxFpWWkBkGkB9mkAaQH9aQGYaQHFaStpfWkAaQdpSUkiGXVQQXkzSE9LRTJ2RU10bnBhNmN3BjsAVEkiD3VtcGx1c3V1aWQGOwBGIiU4ODJiODNkMTY1ZGY4ZTE3ZWU5ZjgyYmJlNWQzOTAxNUkiEF9jc3JmX3Rva2VuBjsARkkiMUQ1QmE2SU5VeGVaZFcyQlZUeUpwUVoyZzRkQzltMDljWmljSFhQbHgvbkE9BjsARkkiD3Nlc3Npb25faWQGOwBUSSIlMTYzZGQwM2Y4Yjk0MDA3YzhkNDlkZGFjNjBmNzE0OTMGOwBG--b0ddb62030d947e7318347ac8302ded32dd7a5e3";
 
     public static HashMap<String, Integer> resultMap;//cid all date
@@ -58,9 +59,14 @@ public class UMHelper {
                 sendResult();
                 sendResult2();
                 sendResult3();
-                sendEmail();
+                int i = 60;
+                if (sb.toString().length() > 10){
+                    sendEmail();
+                } else {
+                    i = 1;
+                }
                 try {
-                    Thread.sleep(1000 * 60 * 60);
+                    Thread.sleep(1000 * 60 * i);
                 } catch (InterruptedException ignored) {
                 }
             }
@@ -75,7 +81,7 @@ public class UMHelper {
         Calendar cal = Calendar.getInstance();
         int hour = cal.get(Calendar.HOUR_OF_DAY);
         Log.i("hour", "" + hour);
-        return hour == 8;
+        return hour == 8 || debug;
     }
 
     private static void sendResult() {
@@ -84,11 +90,11 @@ public class UMHelper {
         resultMap = sort(resultMap);
 
         Set<Map.Entry<String, Integer>> entry = resultMap.entrySet();
-        sb.append("======================各个渠道的用户总数 start=====================</br>");
+        sb.append("======================\u5404\u4e2a\u6e20\u9053\u7684\u7528\u6237\u603b\u6570 start=====================</br>");
         for (Map.Entry<String, Integer> item : entry) {
             sb.append(item.getKey()).append(",").append(item.getValue()).append("</br>");
         }
-        sb.append("======================各个渠道的用户总数 end=====================</br>");
+        sb.append("======================\u5404\u4e2a\u6e20\u9053\u7684\u7528\u6237\u603b\u6570 end=====================</br>");
     }
 
     private static HashMap<String, Integer> sort(HashMap<String, Integer> map) {
@@ -117,18 +123,18 @@ public class UMHelper {
         appResultMap = sort(appResultMap);
 
         Set<Map.Entry<String, Integer>> entry = appResultMap.entrySet();
-        sb.append("======================各个游戏的用户总数 start=====================</br>");
+        sb.append("======================\u5404\u4e2a\u6e38\u620f\u7684\u7528\u6237\u603b\u6570 start=====================</br>");
         for (Map.Entry<String, Integer> item : entry) {
             sb.append(item.getKey()).append(",").append(item.getValue()).append("</br>");
         }
-        sb.append("======================各个游戏的用户总数 end=====================</br>");
+        sb.append("======================\u5404\u4e2a\u6e38\u620f\u7684\u7528\u6237\u603b\u6570 end=====================</br>");
     }
 
     private static void sendResult3() {
         if (null == appCidDetailMap || appCidDetailMap.size() == 0) return;
 
         Set<Map.Entry<String, HashMap<String, Integer>>> entry = appCidDetailMap.entrySet();
-        sb.append("======================各个游戏各自渠道的用户总数 start============</br>");
+        sb.append("======================\u5404\u4e2a\u6e38\u620f\u5404\u81ea\u6e20\u9053\u7684\u7528\u6237\u603b\u6570 start============</br>");
         for (Map.Entry<String, HashMap<String, Integer>> item : entry) {
             sb.append(item.getKey()).append(":</br>");
             HashMap<String, Integer> tmp = sort(item.getValue());
@@ -139,7 +145,7 @@ public class UMHelper {
                 }
             }
         }
-        sb.append("======================各个游戏各自渠道的用户总数 end============</br>");
+        sb.append("======================\u5404\u4e2a\u6e38\u620f\u5404\u81ea\u6e20\u9053\u7684\u7528\u6237\u603b\u6570 end============</br>");
     }
 
     private static void sendEmail() {
@@ -166,7 +172,7 @@ public class UMHelper {
             AppsDetailRes appsDetailRes = new Gson().fromJson(res, AppsDetailRes.class);
             if (null != appsDetailRes) {
                 String result = appsDetailRes.getResult();
-                int total = appsDetailRes.getTotal();//游戏总数
+                int total = appsDetailRes.getTotal();//娓告垙鎬绘暟
                 List<AppsDetailRes.StatsBean> stats = appsDetailRes.getStats();
                 if ("success".equals(result) && total > 0 && null != stats) {
                     for (AppsDetailRes.StatsBean item : stats) {
@@ -194,7 +200,7 @@ public class UMHelper {
             AppChannelDetailRes appChannelDetailRes = new Gson().fromJson(res, AppChannelDetailRes.class);
             if (null != appChannelDetailRes) {
                 String result = appChannelDetailRes.getResult();
-                int total = appChannelDetailRes.getTotal();//渠道总数
+                int total = appChannelDetailRes.getTotal();//娓犻亾鎬绘暟
 
                 if (null == appCidDetailMap) {
                     appCidDetailMap = new TreeMap<>();
@@ -236,7 +242,7 @@ public class UMHelper {
             URL url = new URL(urlStr);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
-            // 设置请求体的类型
+            // 璁剧疆璇锋眰浣撶殑绫诲瀷
 //            httpURLConnection.setRequestProperty("Connection", "keep-alive");
 //            httpURLConnection.setRequestProperty("Accept", "application/json, text/javascript, */*; q=0.01");
 //            httpURLConnection.setRequestProperty("X-CSRF-Token", "D5Ba6INUxeZdW2BVTyJpQZ2g4dC9m09cZicHXPlx/nA=");
@@ -250,12 +256,12 @@ public class UMHelper {
 
             httpURLConnection.connect();
 
-            // 获得服务器响应的结果和状态码
+            // 鑾峰緱鏈嶅姟鍣ㄥ搷搴旂殑缁撴灉鍜岀姸鎬佺爜
             int responseCode = httpURLConnection.getResponseCode();
             if (responseCode != 200) {
                 return null;
             }
-            BufferedReader is = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            BufferedReader is = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), ENCODING));
             String line;
             StringBuffer sb = new StringBuffer();
             while ((line = is.readLine()) != null) {
